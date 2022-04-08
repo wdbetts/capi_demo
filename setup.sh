601d532fa820e3cc72902fb7521b7b12230f24a4
David Betts <wbetts@twilio.com>
2022-04-08 17:01:07 -0600

    Add cluster class

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 kind create cluster --config mgmt-cluster-config.yaml --name mgmt
 
 # Install Cluster API into the cluster
+export CLUSTER_TOPOLOGY=true
 clusterctl init --infrastructure docker
 
 # Install Argo CD
@@ -20,10 +21,17 @@ https://localhost:8080/
 # Just to look at the structure
 clusterctl generate cluster c1 --flavor development \
   --infrastructure docker \
-  --kubernetes-version v1.21.1 \
+  --kubernetes-version v1.23.3 \
   --control-plane-machine-count=3 \
   --worker-machine-count=3 \
   > c1-clusterapi.yaml
+
+clusterctl generate cluster c1 --flavor development-topology \
+  --infrastructure docker \
+  --kubernetes-version v1.23.3 \
+  --control-plane-machine-count=3 \
+  --worker-machine-count=3 \
+  > c1-clusterapi_clusterclass.yaml
 
 # Delete c1-clusterapi.yaml, we'll use Helm charts in mgmt/ instead
 rm c1-clusterapi.yaml
@@ -34,10 +42,17 @@ kubectl apply -f argoapp-c2-cluster-create.yaml
 
 kind export kubeconfig --name c1
 sed -i '' 's/0.0.0.0:/127.0.0.1:/' ~/.kube/config
+# Point the kubeconfig to the exposed port of the load balancer, rather than the inaccessible container IP.
+sed -i -e "s/server:.*/server: https:\/\/$(docker port c1-lb 6443/tcp | sed "s/0.0.0.0/127.0.0.1/")/g" ./capi-quickstart.kubeconfig
 
-kubectl apply -f https://docs.projectcalico.org/v3.20/manifests/calico.yaml --context kind-c1
+kubectl apply -f https://docs.projectcalico.org/v3.21/manifests/calico.yaml --context kind-c1
+kubectl --kubeconfig=./capi-quickstart.kubeconfig \
+  apply -f https://docs.projectcalico.org/v3.21/manifests/calico.yaml
 
 kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
 kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
 
 argocd login localhost:8080
+
+kubectl delete cluster c1
+kind delete clusters mgmt
